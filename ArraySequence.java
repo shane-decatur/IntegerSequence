f142389dba5514542d59fbe362f8f16983fba2d0
shane-decatur <sdecatur20@stuy.edu>
2020-12-07 13:46:23 -0500

    added constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,4 +1,27 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
-  
+  int currentIndex;
+  int[] data;
+
+  public ArraySequence(int[] other){
+    currentIndex = 0;
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i++){
+      data[i] = other[i];
+    }
+  }
+
+  public boolean hasNext(){
+    return false;
+  }
+
+  public int next(){
+    return 0;
+  }
+
+  public int length(){
+    return 0;
+  }
+
+  public void reset(){ }
 }
